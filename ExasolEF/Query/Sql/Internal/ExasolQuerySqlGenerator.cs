3029eb3f2612241672c4d23f50a1b604804da67d
Evgeny Veprikov <evgeny@vista.aero>
2018-04-20 18:17:34 +0300

    Первая версия

---
+++ ExasolEF/Query/Sql/Internal/ExasolQuerySqlGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
+using Microsoft.EntityFrameworkCore.Query.Sql;
+
+namespace ExasolEF_NetCore.Query.Sql.Internal
+{
+    public class ExasolQuerySqlGenerator : DefaultQuerySqlGenerator
+    {
+        protected ExasolQuerySqlGenerator([NotNull] QuerySqlGeneratorDependencies dependencies, [NotNull] SelectExpression selectExpression) : base(dependencies, selectExpression)
+        {
+        }
+    }
+}
